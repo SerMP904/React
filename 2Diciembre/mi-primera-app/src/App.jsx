fec33567b91f05a5d0df04ee439ed365c20b8fdc
Sergio Martinez <sermarpad@gmail.com>
2025-12-02 13:28:22 +0100

    removed ejercicios

--- 2Diciembre/mi-primera-app/src/App.jsx
+++ 2Diciembre/mi-primera-app/src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css'
 import FraseCelebreComponent from './components/FraseCelebreComponent'
 import HolaMundoComponent from './components/holaMundoComponent'
+import PresentacionComponent from './components/PresentacionComponent'
 
 function App() {
   const titulo = "El Arte de la Guerra"
   const subtitulo ="Sun Tzu"
   return (
-    <>
+    <div>
     <p>{titulo}</p>
     <hr />
     <p>{subtitulo}</p>
     <hr />
     <HolaMundoComponent />
-    <HolaMundoComponent />
+    <PresentacionComponent />
     <HolaMundoComponent />
     <hr />
     <FraseCelebreComponent />
-    </>
+    </div>
+    
   )
 }
 
---
+++ 2Diciembre/mi-primera-app/src/components/PresentacionComponent.jsx
@@ -0,0 +1,10 @@
+const PresentacionComponent = () => {
+    return (
+      <div>
+        <h2>Presentación</h2>
+        <p>Mi nombre es [tu nombre] y este es mi primer componente en React.</p>
+      </div>
+    )
+  }
+
+export default PresentacionComponent
