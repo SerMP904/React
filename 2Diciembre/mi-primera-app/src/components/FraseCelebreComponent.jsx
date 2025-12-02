82216117b387daf7d1797de1630e58518a1acf83
Sergio Martinez <sermarpad@gmail.com>
2025-12-02 12:18:55 +0100

    created second-app

---
+++ 2Diciembre/mi-primera-app/src/components/FraseCelebreComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FraseCelebreComponent = () => {
+  return (
+    <div>Los químicos dicen "Ácido un placer". Los andaluces dicen "Á cido un plaser"</div>
+  )
+}
+
+export default FraseCelebreComponent
---
+++ 2Diciembre/mi-primera-app/src/components/HolaMundoComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HolaMundoComponent = () => {
+  return (
+    <div>HolaMundo</div>
+  )
+}
+
+export default HolaMundoComponent
