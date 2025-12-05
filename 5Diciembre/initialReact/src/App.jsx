d141b51777c60d8e950acc2da610955dcaf5ee46
sergioM <sermarpad@gmail.com>
2025-12-05 10:32:03 +0100

    created 5 dic

---
+++ 5Diciembre/initialReact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import AutoCounter from './components/AutoCounterComponent'
+
+function App() {
+
+  return (
+    <>
+    <AutoCounter/>
+    </>
+  )
+}
+
+export default App
